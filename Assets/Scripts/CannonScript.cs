12003e565d8693c90eb27efab3b67f1ffa64ae35
Ricardo <ricardoneves93@hotmail.com>
2015-10-28 18:01:52 +0000

    Converted javascript to C# and added CNControls package

---
+++ Assets/Scripts/CannonScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonScript : MonoBehaviour {
+	
+	public GameObject cannonBall;
+	public GameObject barrel;
+	public GameObject endPiece;
+	public GameObject aim;
+	public float rateOfFire;
+	private float fireDelay;
+	private int speed;
+	
+	
+	void Start() {
+		
+		rateOfFire = 0.5F;
+		speed = 50;
+		fireDelay = 0.0F;
+		barrel.transform.localEulerAngles = new Vector3(0, barrel.transform.localEulerAngles.y, barrel.transform.localEulerAngles.z);
+	}
+	
+	
+	void Update() {
+		
+		if(Input.GetButtonDown("Fire1")) {
+			
+			fireDelay = Time.time + rateOfFire;
+			
+			GameObject clone = (GameObject) Instantiate(cannonBall, new Vector3(endPiece.transform.position.x, endPiece.transform.position.y, endPiece.transform.position.z), cannonBall.transform.rotation);
+			
+			
+			Vector3 dir = aim.transform.forward;
+			Rigidbody rb = clone.GetComponent<Rigidbody>();
+			rb.velocity = new Vector3(dir.x * speed, dir.y * speed, dir.z * speed);
+			
+		}
+		
+		float rotation = Input.GetAxis("Vertical");
+
+		print ("X: " + barrel.transform.localEulerAngles.x);
+		
+		if(barrel.transform.localEulerAngles.x < 315 && barrel.transform.localEulerAngles.x >= 310)
+			barrel.transform.localEulerAngles = new Vector3(315, barrel.transform.localEulerAngles.y, barrel.transform.localEulerAngles.z);
+		
+		
+		if(barrel.transform.localEulerAngles.x >= 0 && barrel.transform.localEulerAngles.x <= 5)
+			barrel.transform.localEulerAngles = new Vector3(0, barrel.transform.localEulerAngles.y, barrel.transform.localEulerAngles.z);
+		
+		
+		if(barrel.transform.localEulerAngles.x == 0 || (barrel.transform.localEulerAngles.x >= 315 && barrel.transform.localEulerAngles.x <= 360)) {
+			barrel.transform.Rotate(new Vector3(-rotation, barrel.transform.localEulerAngles.y, barrel.transform.localEulerAngles.z));
+			print ("rotate");
+		}
+		
+	}
+	
+	
+}
